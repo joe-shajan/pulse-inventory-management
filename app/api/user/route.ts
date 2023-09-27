9937a26d837f1e9e0d3de2a90a403c0301962151
joe-shajan <joeshajan1551@gmail.com>
2023-09-28 00:38:48 +0530

    prisma setup

---
+++ app/api/user/route.ts
@@ -0,0 +1,42 @@
+import prisma from "@/utils/prisma";
+
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  try {
+    console.log("in route");
+
+    const users = await prisma.user.findMany();
+    console.log(users);
+
+    return NextResponse.json(users);
+  } catch (error: any) {
+    console.log("error");
+    return NextResponse.json(
+      {
+        error: error.message,
+        errorCode: error.code,
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    const data = await request.json();
+    console.log(data);
+    const user = await prisma.user.create({ data });
+    console.log(user);
+
+    return NextResponse.json(user);
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        error: error.message,
+        errorCode: error.code,
+      },
+      { status: 500 }
+    );
+  }
+}
