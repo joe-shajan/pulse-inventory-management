8d8bd1ded6fb6ae8ccc2239c4b0fd742fc0b3193
joe-shajan <joeshajan1551@gmail.com>
2023-10-02 17:01:38 +0530

    fix: delete shop container file

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,29 @@
-import { ShopContainer } from "@/components/shop/ShopContainer";
+"use client";
 
-export default async function Home() {
-  // async function getUsers() {
-  //   return (await axios.get("/api/user").then((data) => data.data)) as User[];
-  // }
+import { Button } from "@/components";
+import Modal from "@/components/Modal";
+import { CreateShop } from "@/components/shop/CreateShop";
+import ShopCard from "@/components/shop/ShopCard";
+import useModal from "@/hooks/useModal";
 
-  // const { data: users } = useQuery<User[]>({
-  //   queryKey: ["stream-hydrate-users"],
-  //   queryFn: () => getUsers(),
-  //   // suspense: true,
-  //   // staleTime: 5 * 1000,
-  // });
-
-  // const mutation = useMutation({
-  //   mutationFn: (user: User) => {
-  //     return axios.post("/api/user", user);
-  //   },
-  // });
+export default function Home() {
+  const { isOpen, toggle } = useModal();
 
   return (
     <>
-      <ShopContainer />
+      <Modal isOpen={isOpen} toggle={toggle}>
+        <CreateShop toggle={toggle} />
+      </Modal>
+      <div className="flex justify-end px-4 md:px-12 lg:px-28 mt-6">
+        <Button onClick={toggle}>Create New Shop</Button>
+      </div>
+      <div className="container my-4 mx-auto px-4 md:px-12 lg:px-28">
+        <div className="flex flex-wrap -mx-1 lg:-mx-4">
+          {[1, 2, 3, 4, 5, 6, 7].map((num) => (
+            <ShopCard key={num} />
+          ))}
+        </div>
+      </div>
     </>
   );
 }
