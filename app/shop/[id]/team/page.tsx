a3cb12b4ad770ecd3106624b3d90c14593127070
joe-shajan <joeshajan1551@gmail.com>
2023-10-02 21:54:05 +0530

    feat: get user role

--- app/shop/[id]/team/page.tsx
+++ app/shop/[id]/team/page.tsx
@@ -8,6 +8,7 @@ import UsersTable from "@/components/team/TeamMembersTable";
 import { AddTeamMember } from "@/components/team/TeamMember";
 import { useQuery } from "@tanstack/react-query";
 import { getTeamMembers } from "@/services/TeamsServices";
+import { getUserRole } from "@/services";
 
 export default function Page({ params }: any) {
   const { id } = params;
@@ -23,18 +24,28 @@ export default function Page({ params }: any) {
     queryFn: () => getTeamMembers(id),
   });
 
+  const { data: userRole } = useQuery({
+    queryKey: ["userRole"],
+    queryFn: () => getUserRole(id),
+  });
+
   if (error) {
     return <div>Could not fetch team members</div>;
   }
 
   return (
     <>
-      <Modal isOpen={isOpen} toggle={toggle}>
-        <AddTeamMember toggle={toggle} />
-      </Modal>
-      <div className="flex gap-3 justify-end px-4 md:px-12 lg:px-28 mt-6 mb-2">
-        <Button onClick={toggle}>Add team member</Button>
-      </div>
+      {userRole === "ADMIN" ? (
+        <>
+          <Modal isOpen={isOpen} toggle={toggle}>
+            <AddTeamMember toggle={toggle} />
+          </Modal>
+          <div className="flex gap-3 justify-end px-4 md:px-12 lg:px-28 mt-6 mb-2">
+            <Button onClick={toggle}>Add team member</Button>
+          </div>
+        </>
+      ) : null}
+
       {isLoading ? (
         <div className="container my-2 mx-auto px-4 md:px-12 lg:px-28">
           loading team members...
