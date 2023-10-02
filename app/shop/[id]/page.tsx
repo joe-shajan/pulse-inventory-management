1a9519c2276ef0eb29a04eaa243ebc9a376651ab
joe-shajan <joeshajan1551@gmail.com>
2023-10-02 16:06:15 +0530

    feat: table ui

--- app/shop/[id]/page.tsx
+++ app/shop/[id]/page.tsx
@@ -14,11 +14,11 @@ export default async function Page({ params }: any) {
 
       <div className="container my-2 mx-auto px-4 md:px-12 lg:px-28">
         <table className="w-full flex flex-row r flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
-          <thead className="text-gray-700">
+          <thead className="text-gray-700 ">
             {[1, 2, 3, 4].map((i) => (
               <tr
                 key={i}
-                className="bg-slate-100 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0"
+                className="bg-slate-100 border-grey-light md:border-0 border border-r-0 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0"
               >
                 <th className="p-3 text-left">Name</th>
                 <th className="p-3 text-left">Description</th>
@@ -33,7 +33,7 @@ export default async function Page({ params }: any) {
             {[1, 2, 3, 4].map((i) => (
               <tr
                 key={i}
-                className={`flex flex-col flex-no wrap sm:table-row mb-2 border-grey-light md:border-b border-0 sm:mb-0 hover:bg-slate-100 ${
+                className={`flex rounded-lg rounded-s-none flex-col flex-no wrap sm:table-row mb-2 border-grey-light md:border-b border border-l-0 sm:mb-0 hover:bg-slate-100 ${
                   i % 2 === 0 ? "bg-slate-50" : ""
                 }`}
               >
