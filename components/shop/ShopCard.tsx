75fc869dca71353c81462f389cc39d6ffb7ab464
joe-shajan <joeshajan1551@gmail.com>
2023-10-06 00:02:34 +0530

    shop card bg

--- components/shop/ShopCard.tsx
+++ components/shop/ShopCard.tsx
@@ -10,7 +10,7 @@ const ShopCard = ({ shop }: Props) => {
   return (
     <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
       <Link href={`/shop/${shop.id}`}>
-        <article className="overflow-hidden rounded-lg shadow-lg hover:shadow-xl border-slate-200 border p-3 md:p-0 text-slate-700">
+        <article className="bg-slate-50 overflow-hidden rounded-lg shadow-lg hover:shadow-xl border-slate-200 border p-3 md:p-0 text-slate-700">
           <header className="leading-tight p-2">
             <h1 className="text-xl font-semibold text-center">{shop.name}</h1>
             <p className="text-grey-darker text-sm mt-4 px-0 md:px-2 ">
