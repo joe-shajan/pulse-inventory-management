1843d72a89d6a006b9d79c9080ff68ee97a03c40
joe-shajan <joeshajan1551@gmail.com>
2023-10-05 21:49:25 +0530

    fix: ui fixes

--- components/shop/ShopCard.tsx
+++ components/shop/ShopCard.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Shop } from "@/types";
@@ -8,23 +7,27 @@ type Props = {
 };
 
 const ShopCard = ({ shop }: Props) => {
-  console.log(shop);
-
   return (
     <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
       <Link href={`/shop/${shop.id}`}>
-        <article className="overflow-hidden rounded-lg shadow-lg hover:shadow-xl border-slate-200 border p-3 md:p-0">
-          <header className=" leading-tight p-2 md:p-4">
-            <h1 className="text-lg">
-              <a className="no-underline hover:underline text-black" href="#">
-                Name: {shop.name}
-              </a>
-            </h1>
-            <p className="text-grey-darker text-sm mt-2">Bio: {shop.bio}</p>
+        <article className="overflow-hidden rounded-lg shadow-lg hover:shadow-xl border-slate-200 border p-3 md:p-0 text-slate-700">
+          <header className="leading-tight p-2">
+            <h1 className="text-xl font-semibold text-center">{shop.name}</h1>
+            <p className="text-grey-darker text-sm mt-4 px-0 md:px-2 ">
+              Bio: <span className="font-semibold">{shop.bio}</span>
+            </p>
           </header>
 
-          <footer className="flex items-center justify-between leading-none p-2 md:p-4">
-            <p className="text-sm">Place: {shop.address}</p>
+          <footer className="flex flex-col leading-none px-2 md:px-4 pb-2 ">
+            <p className="text-sm">
+              Place: <span className="font-semibold">{shop.address}</span>
+            </p>
+            <p className="text-sm">
+              Latitude: <span className="font-semibold">{shop.latitude}</span>
+            </p>
+            <p className="text-sm">
+              Longitude: <span className="font-semibold">{shop.longitude}</span>
+            </p>
           </footer>
         </article>
       </Link>
